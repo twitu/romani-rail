5d1e412fb4da5a8dcbb84808bde4c0ecfd3536d5
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-04-29 18:44:18 -0400

    Fix distance query

--- railway_query_web.js
+++ railway_query_web.js
@@ -17,14 +17,14 @@ ORDER BY train_schedule.seq ASC;
 function dstStnQry(srcStn) {
   return `
 WITH evening_trains AS (
-  SELECT ts.train_no, tr.train_name, tr.destination_station, ts.departure_time AS start_time, ts.seq AS start_seq, ts.station_code AS start_station
+  SELECT ts.train_no, tr.train_name, tr.destination_station, ts.departure_time AS start_time, ts.seq AS start_seq, ts.station_code AS start_station, ts.distance as start_distance
   FROM train_schedule ts
   JOIN trains tr ON ts.train_no = tr.train_no
   WHERE ts.station_code = "${srcStn}"
     AND ts.departure_time BETWEEN '19:00:00' AND '22:00:00'
 ),
 morning_stations AS (
-  SELECT ts.train_no, ts.station_code, ts.arrival_time AS end_time, ts.seq AS end_seq
+  SELECT ts.train_no, ts.station_code, ts.arrival_time AS end_time, ts.seq AS end_seq, ts.distance AS end_distance
   FROM train_schedule ts
   WHERE ts.arrival_time BETWEEN '06:00:00' AND '10:00:00'
 ),
@@ -33,7 +33,7 @@ trains_with_travel_time AS (
     (strftime('%s', ms.end_time) - strftime('%s', et.start_time) +
       (CASE WHEN ms.end_time < et.start_time THEN 86400 ELSE 0 END)) / 3600.0 AS travel_time
   FROM morning_stations ms
-  JOIN evening_trains et ON ms.train_no = et.train_no AND ms.end_seq > et.start_seq
+  JOIN evening_trains et ON ms.train_no = et.train_no AND ms.end_seq > et.start_seq AND ms.end_distance - et.start_distance < 1000
 ),
 final_result AS (
   SELECT DISTINCT twtt.station_code, twtt.train_no, twtt.train_name, twtt.destination_station, twtt.start_station
